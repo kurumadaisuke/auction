7adfd934ddfaf5b9deeafbaf49d18fa0638313b8
kurumadaisuke <kuruma0909@gmail.com>
2020-03-08 20:43:41 +0900

    add バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  vilidates :name, presence: true, lenght: (maximum: 50)
+  vilidates :description, presence: true, lenght: (maximum: 2000)
+  vilidates :price, presence: true, numericality: (only_integer: true greater_than_or_equal_to: 1)
+
 end
