5e78877824f11b83afef1114f504a4c8e93f317e
kurumadaisuke <kuruma0909@gmail.com>
2020-03-08 10:48:20 +0900

    詳細画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+# get '/items' => 'index#index'
+# post '/items' => 'index#create'
+# get '/items' => 'index#new'
+
+resources :items
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
